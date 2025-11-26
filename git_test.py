20431f75bd0531931836173693f0944a862a6a41
Roshan Dhami <roshan.dhami@edu.turkuamk.fi>
2025-11-26 22:33:42 +0200

    modified code and added file

--- git_test.py
+++ git_test.py
@@ -2,4 +2,8 @@
 # Author: Roshan Dhami
 # Description: Code for Git testing purposes
 
-print("I am quite familiar with Git")
+print("I am familiar with Git")
+ 
+print("This is a test file for Git integration.")
+print("Let's see how Git handles this file.")
+
