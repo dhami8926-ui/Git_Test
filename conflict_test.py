ffdef0bffa42b0628e9f6c47ea0c9c76fa781e44
Roshan_Dhami <roshan.dhami@edu.turkuamk.fi>
2026-01-15 18:35:22 +0200

    Initial commit

--- conflict_test.py
+++ conflict_test.py
@@ -1,1 +1,4 @@
 print("Conflict version")
+print("Main Version")
+print("Feature Version")
+print("Another Feature Version")
--- dev_test.py
+++ dev_test.py
@@ -1,12 +1,1 @@
-print("Hello World")
-print("Welcome to git stash")
-
-a = 10
-b = 20 
-c = a + b
-print("Sum:", c)
-
-print("This is new commit.")
-
-
 
